888a88c1a59a02523c71b397d94a9fa2c919ec38
Michał Kowalik <31324118+KowalikMichal@users.noreply.github.com>
2018-06-26 08:06:01 +0200

    without add to sharepoint

---
+++ AddItems.js
@@ -0,0 +1,154 @@
+var EWODeferred = new $.Deferred();
+var VAADeferred = new $.Deferred();
+var ApprovalDeferred = new $.Deferred();
+var AdministratorDeferred = new $.Deferred();
+var MailsReceivers= {'TO': [], 'CC': []};
+
+function addItemsToSharePoint(){
+	$('#ModalInfo :button').hide()
+	$('#ModalInfo').find('.icon-box').append('<div class="loader"></div>')
+	
+	$('#ModalInfo').find('.modal-header').addClass('Working');
+	$('#ModalInfoBody').html('<h4>Working on it!</h4><p>Please give me a moment...</p>');
+	$('#ModalInfo').modal({backdrop: "static"});
+
+	addToEWOList();
+//if static torque just add item to static torque part
+
+// if VAA add to new list VAA
+
+// add to EWO Cost, Approval List and Administrator List
+	$.when(EWODeferred).done(function(){
+		$('#ModalInfo :button').show()
+		$('#ModalInfo').find('.icon-box').html('<i class="glyphicon glyphicon-ok"></i>');
+		$('#ModalInfo').find('.modal-header').removeClass('Working').addClass('Successful');
+		$('#ModalInfoBody').html('<h4>Great!</h4><p>EWO has been created successfully.</p>');
+		
+		$('#ModalInfo').on('click', function(){
+			location.reload();
+		});
+
+		$('#ModalInfo').modal({backdrop: "static"});
+	}).fail(function(){
+		$('#ModalInfo :button').show()
+		$('#ModalInfo').find('.icon-box').html('<i class="glyphicon glyphicon-remove"></i>');
+		$('#ModalInfo').find('.modal-header').removeClass('Working').addClass('Error');
+		$('#ModalInfoBody').html('<h4>Ooops!</h4><p>Something went wrong :(</p>');
+		
+		$('#ModalInfo').on('click', function(){
+			location.reload();
+		});
+
+		$('#ModalInfo').modal({backdrop: "static"})
+		});
+}
+
+function addToEWOList() {
+	getUserInfo();
+	deferred.done(function(userMail) {
+		var user = SP.FieldUserValue.fromUser(userMail);
+		var clientContext = new SP.ClientContext.get_current();	
+		var oList = clientContext.get_web().get_lists().getByTitle('EWOList');
+		var itemCreateInfo = new SP.ListItemCreationInformation();
+		this.oListItem = oList.addItem(itemCreateInfo);
+// ADD COLUMNS
+		oListItem.set_item('Title', 'Example title');
+		oListItem.set_item('Initiator', userMail);
+		
+		oListItem.update();
+		clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceededaddToEWOList), Function.createDelegate(this, this.onQueryFailedaddToEWOList));
+	});
+}
+
+function onQuerySucceededaddToEWOList() {
+	EWODeferred.resolve();
+}
+
+function onQueryFailedaddToEWOList(sender, args) {
+	console.log('addToEWOList failed: ' + args.get_message() + '\n' + args.get_stackTrace());
+	EWODeferred.reject();
+}
+
+function getUserInfo(peoplePickerElementId) {
+	//var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDRE_TopSpan;
+
+	var users = peoplePickerElementId.GetAllUserInfo();
+	var userMail = [];
+	
+	$.each(users, function(index, element){
+		 $.when(GetUserIdFromUserName(element.Key)).done(function(data){
+			$.map(data, function(n){
+				userMail.push(SP.FieldUserValue.fromUser(n.Email));
+			});
+			if (userMail.length == users.length) return deferred.resolve(userMail);
+		});
+	});
+}
+//new
+function appendSummaryMails(peoplePickerElementId) {
+	//var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDRE_TopSpan;
+
+	var users = peoplePickerElementId.GetAllUserInfo();
+	var userMail = [];
+	
+	$.each(users, function(index, element){
+		 $.when(GetUserIdFromUserName(element.Key)).done(function(data){
+			$.map(data, function(n){
+				MailsReceivers['CC'].push(SP.FieldUserValue.fromUser(n.Email));
+			});
+		});
+	});
+}
+
+function GetUserIdFromUserName(userName) {
+		var siteUrl = _spPageContextInfo.siteAbsoluteUrl;
+		var accountName = userName;
+		return $.ajax({
+					url: siteUrl + "/_api/web/siteusers(@v)?@v='" + 
+						encodeURIComponent(accountName) + "'",
+					method: "GET",
+					headers: { "Accept": "application/json; odata=verbose" },
+					success: function (data) {
+						return data.d.Emai;
+					},
+					error: function (data) {
+						console.log(JSON.stringify(data));
+					}
+				});
+}
+
+// UNDER CONSTRUCTION
+//********************
+//*******************
+function createMailToVAA(){
+	var addresses = "marcin.plisz@opel.com;";
+	var body = "Hello," +"<br/>"+
+	"Please check the attached WO and send your comment ASAP to Jacek Pedrycz."+"<br/>"+
+	"For further question please contact to:" + $("#nameDRE").val() + "<"+ $("#mailDRE").val()+">"; 
+	var subject = "EWO " + $("#numberEWO").val();
+	
+	var href = "mailto:" + addresses + "?"
+		 + "subject=" + subject + "&"
+		 + "body=" + body;
+	var wndMail;
+	wndMail = window.open(href, "_blank", "scrollbars=yes,resizable=yes,width=10,height=10");
+	if(wndMail){
+		wndMail.close();    
+	}
+}
+
+
+/*
+for(var key in SPClientPeoplePicker.SPClientPeoplePickerDict){
+   //for every peoplepicker on the page
+   var picker = SPClientPeoplePicker.SPClientPeoplePickerDict[key];
+   //check out the peoplepicker object - there's a lot more you can do with it
+   console.log(picker);
+
+   //haven't found out what the second parameter does actually but it works like this 
+   picker.BatchAddUserKeysOperation(['domain\\loginname'], 0);
+}
+
+
+
+*/
---
+++ EWO_FirstData.js
@@ -0,0 +1,606 @@
+	var engineersForEWO={planner:[],processor:[],gl:[],list:""};
+	var actualSelection;
+//used for final engineers matching
+	var mailReceivers={mail:[]}; //possible to add name
+	var cc={mail:[]};//possible to add name
+	
+	var platforms=[]; //actually workbookw
+	var platformCoordinators=[]; //guys for given workbooks
+
+	var plannersAndGL={planner:[],gl:[]};
+
+	var deferred = new $.Deferred();
+	var counter = null;
+
+$(function(){
+	SP.SOD.executeFunc('sp.js', 'SP.ClientContext',function(){
+		var InitializePeoplePicker = {'peoplePickerDRE' : 'generalInfoOfEWO', 'peoplePickerMePlaner' : 'planner', 'peoplePickerMPDProcessor': 'processor', 'peoplePickerMeGL': 'GL', 'peoplePickermailReceivers': 'mailReceivers', 'peoplePickermailMailCopy': 'mailCopy', 'peoplePickermailPlatformCoordinators': 'platformCoordinators'};
+
+		$.when($.each(InitializePeoplePicker, function(key, ClassName){
+			if (key == 'peoplePickerDRE') initializePeoplePicker(key, false, ClassName)
+			else initializePeoplePicker(key, true, ClassName);
+		})).done(function(){
+			readyFunction();
+		})
+	});
+});
+
+//// people picker
+function initializePeoplePicker(peoplePickerElementId, AllowMultipleValues, customClass) {
+	var schema = {};
+	schema['PrincipalAccountType'] = 'User,DL,SecGroup,SPGroup';
+	schema['SearchPrincipalSource'] = 15;
+	schema['ResolvePrincipalSource'] = 15;
+	schema['AllowMultipleValues'] = AllowMultipleValues;
+	schema['MaximumEntitySuggestions'] = 50;
+	schema['Width'] = '100%';
+
+	this.SPClientPeoplePicker_InitStandaloneControlWrapper(peoplePickerElementId, null, schema);
+	$('#'+peoplePickerElementId+'_TopSpan').addClass(customClass);
+	$('#'+peoplePickerElementId+'_TopSpan').addClass('form-control');
+}
+/*
+function getUserInfo() {
+	var peoplePicker = this.SPClientPeoplePicker.SPClientPeoplePickerDict.peoplePickerDiv_TopSpan;
+
+	var users = peoplePicker.GetAllUserInfo();
+	var userInfo = '';
+	for (var i = 0; i < users.length; i++) {
+		var user = users[i];
+		for (var userProperty in user) { 
+			userInfo += userProperty + ':  ' + user[userProperty] + '<br>';
+		}
+	}
+	var keys = peoplePicker.GetAllUserKeys();
+	getUserId(users[0].Key);
+}
+
+// Get the user ID.
+function getUserId(loginName) {
+	var context = new SP.ClientContext.get_current();
+	this.user = context.get_web().ensureUser(loginName);
+	context.load(this.user);
+	context.executeQueryAsync(
+		 Function.createDelegate(null, ensureUserSuccess), 
+		 Function.createDelegate(null, onFail)
+	);
+}
+*/
+function ensureUserSuccess() {
+	console.log(this.user.get_email());
+}
+
+function onFail(sender, args) {
+	alert('Query failed. Error: ' + args.get_message());
+}
+
+	
+function readyFunction(){
+	$('#numberEWO').focusout(function(){
+		checkEWO('EWO%20List', $('#numberEWO').val());
+	});
+
+	$(".generalInfoOfEWO").change(function(){
+		hidePadData();
+	});
+	
+	$("#checkGeneralData").click(function(){ 
+		hidePadData();
+		validateNumber();	
+	});
+	
+	$("#addDRE").click(function(){ 
+		getDataToAddDRE($("#nameDRE").val(),$("#mailDRE").val());
+	});
+	
+	$('body').on('click', '.addSection', function(){
+		if (counter == null) counter = 1;
+		console.log(counter);
+		clonePadSection(counter);
+		counter++;
+	}); 
+	
+	$('body').on('click', '#doSummary', function(){
+		createSummary();
+	}); 
+
+
+	$('body').on('click', '.removeSection', function(){
+		if ($(this).closest(".section").hasClass('clone')){ 
+			//do nothing
+		}else if ($('.removeSection').length > 1){
+			$(this).parent().fadeOut(300, function(){
+				$(this).parent().parent().remove();
+				return false;
+			})
+		}else{
+			alert("At least one PAD section needs to be fulfilled!");
+		}
+		return false;
+	});
+	
+	$(".PAD-analysis" ).on( "change", ".workbook", function() {
+		var platformAndMY=checkPlatform(this.value);
+		var currentRowPAD=$(this).parents(".row");
+		currentRowPAD.find(".platform").val(platformAndMY.platform);
+		currentRowPAD.find(".modelYear").val(platformAndMY.MY);
+	});
+  
+	$('#staticTorque').on('click',function (){
+		if ($("#staticTorque").is(":checked")){
+			$("#titleEWO").val("NIN - Static Torque Update");
+			SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickerDRE'+"_TopSpan"].AddUserKeys("toni.konz@opel.com");
+		}else{
+			$("#titleEWO").val("");
+			$("#nameDRE").val("");
+			SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickerDRE'+"_TopSpan"].DeleteProcessedUser();
+		}
+	});
+	
+	$(".PAD-analysis" ).on( "change", ".numberPAD", function() {
+		actualSelection=$(this);
+		findEngineersFromPAD(this.value , $(this).parents(".row").find(".workbook").val());
+	});
+}
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+	var matches, substringRegex;
+
+	// an array that will be populated with substring matches
+	matches = [];
+
+	// regex used to determine if a string contains the substring `q`
+	substrRegex = new RegExp(q, 'i');
+
+	// iterate through the pool of strings and for any string that
+	// contains the substring `q`, add it to the `matches` array
+	$.each(strs, function(i, str) {
+	  if (substrRegex.test(str)) {
+		matches.push(str);
+	  }
+	});
+	cb(matches);
+  };
+};
+
+function validateNumberLength(selectorID){
+	$( "#validationMessage" ).append( "<p clas='errorInfo'>Number EWO has to have 7 (or 10) digits.</p>" );
+	isWrong(selectorID);
+}
+
+//*************************************************************************
+//********************Fields easy validation ******************************
+//*************************************************************************	
+
+function validateNumber(){
+	$(".alert-danger").children().remove();
+	$(".alert-danger").hide();
+	var selectorID="#numberEWO";
+	
+	setTrimValue(selectorID);
+	removerErrorClass(selectorID);
+	
+	if($(selectorID).val().indexOf("(")==-1){
+		if(!isNaN($(selectorID).val())){
+			if($(selectorID).val().length!=7){
+				validateNumberLength(selectorID);
+			}else{
+				setTrimValue(selectorID);
+			}
+		}else{
+			$("#validationMessage").append("<p clas='errorInfo'>Number EWO has to be number.</p>");
+			isWrong(selectorID);
+	}
+	}else{
+		if($(selectorID).val().length!=10){
+			validateNumberLength(selectorID);
+		}
+		else{
+			setTrimValue(selectorID);
+		}
+ }
+	selectorID="#titleEWO";
+	validateTitle(selectorID);	
+}
+
+function validateTitle(selectorID){
+	removerErrorClass(selectorID);
+	if(!isNaN($(selectorID).val()) ||$(selectorID).val()==""){
+		$("#validationMessage" ).append( "<p clas='errorInfo'>Title shouldn't be only numbers and/or empty </p>");
+		isWrong(selectorID);
+	}else{
+		setTrimValue(selectorID);	
+	}
+	selectorID = "#peoplePickerDRE_TopSpan_HiddenInput";
+	validateDRE(selectorID);
+}
+function validateDRE(selectorID){
+	removerErrorClass(selectorID);
+	
+	if ($('#peoplePickerDRE_TopSpan_HiddenInput').val() == '[]' || $('#peoplePickerDRE_TopSpan_HiddenInput').val() == ''){
+		$("#validationMessage").append("<p clas='errorInfo'>DRE field is empty </p>");
+		isWrong(selectorID);
+	}
+	else {
+		var nameDRE = $.parseJSON($('#peoplePickerDRE_TopSpan_HiddenInput').val());
+			if(nameDRE[0].hasOwnProperty('Description')) checkOveralValidation();
+	}	
+}
+
+function isWrong(ID){
+	$(ID).parent().addClass('has-error');
+}
+
+function removerErrorClass(ID){
+	var checkError = $(ID).parent().hasClass("has-error") ? true: false;
+	if(checkError){
+		$(ID).parent().removeClass('has-error');
+	}
+}
+
+function setTrimValue(ID){
+	if(/^\s/.test($(ID).val())|| /\s$/.test($(ID).val())){
+		var correctValue =$.trim( $(ID).val());
+		$(ID).val(correctValue);
+	}
+}
+
+function isEmpty(str){
+	return !str.replace(/^\s+/g, '').length; 
+}
+function checkOveralValidation(){
+	if(checkErrorClass("#numberEWO") || checkErrorClass("#peoplePickerDRE_TopSpan_HiddenInput") || (checkErrorClass("#titleEWO"))){
+		alert("Check correctness!");
+		$(".alert-danger").show();
+	}
+	else{
+		$(".PAD-analysis").show();
+		$(".summaryBtn").show();
+		if($("#infoVAA").is(":checked")) $(".VAA-info").show();
+	}
+}
+
+function checkErrorClass(ID){
+	if($(ID).parent().hasClass("has-error")){
+		return true;
+	}
+	return false;
+}
+
+function hidePadData(){
+	if ( $(".PAD-analysis").css('display') == "none" ){
+	// element is hidden do nothing
+	}else{
+	$(".PAD-analysis").hide();
+	}
+}
+
+//*************************************************************************
+//********************Check if EWO exist*************************************
+//*************************************************************************
+
+function checkEWO(listName, EWONo) {  
+	var siteUrl = _spPageContextInfo.siteAbsoluteUrl;  
+	var oDataUrl = siteUrl + "/_api/web/lists/getbytitle('"+listName+"')/items?$select=Title&$filter=(n7af eq'"+ EWONo +"')";
+
+	$.ajax({
+		url: oDataUrl,
+		type: "GET",
+		dataType: "json",
+		}).done( function(data){
+			if(data.value.length > 0) alert('EWO number exist on database!')
+		}).fail(function(errMessage){
+			alert('Error in checkEWO: '+errMessage);
+		});
+}
+
+//************************************************************************
+//********************Clone pad section & add remove link*****************
+//************************************************************************
+
+function clonePadSection(counter){
+	var template = null;
+	template = $(".clone").clone().find("input").val("").end();
+	template.removeClass('clone');
+	var tempInitializePeoplePicker = ['peoplePickerMePlaner' + counter, 'peoplePickerMPDProcessor' + counter, 'peoplePickerMeGL' + counter];
+	
+	template.find('#peoplePickerMePlaner').attr('id', tempInitializePeoplePicker[0]);
+	template.find('#peoplePickerMPDProcessor').attr('id', tempInitializePeoplePicker[1]);
+	template.find('#peoplePickerMeGL').attr('id', tempInitializePeoplePicker[2]);
+
+	$(".PAD-analysis").append(template);
+
+	initializePeoplePicker(tempInitializePeoplePicker[0], true,'generalInfoOfEWO planner');
+	initializePeoplePicker(tempInitializePeoplePicker[1], true,'generalInfoOfEWO processor');
+	initializePeoplePicker(tempInitializePeoplePicker[2], true,'generalInfoOfEWO GL');
+}
+
+function checkPlatform(book){
+	var infoPlatformMY={};
+	switch(book)
+	{
+		case "P3/P7":
+		infoPlatformMY.platform="GAMMA P3/P7";
+		infoPlatformMY.MY="2018";
+		break;
+		
+		case "Z":
+		infoPlatformMY.platform="EPSILON";
+		infoPlatformMY.MY="2020";
+		break;
+
+		case "J":
+		case "0M":
+		case "0X": 
+		case "0S":
+		infoPlatformMY.platform="GAMMA";
+		infoPlatformMY.MY="2018";
+		break;
+
+		case "B":
+		case "P(all plants)":
+		case "0W":
+		infoPlatformMY.platform="GAMMA";
+		infoPlatformMY.MY="2018";
+		break;
+		
+		case "9BUO":
+		infoPlatformMY.platform="GAMMA 9Bxx"
+		infoPlatformMY.MY="2020"
+		
+	}
+	return infoPlatformMY;
+}
+
+function findEngineersFromPAD(PAD,platform){
+	$( "#validationMessage" ).empty();
+	$( "#validationMessage" ).hide();
+	
+	var list= $('.PAD-analysis').find(":selected").attr('data-listName')
+	console.log(list);
+	engineersForEWO.planner=[];
+	engineersForEWO.processor=[];
+	engineersForEWO.gl=[];
+	engineersForEWO.list="";
+	
+	engineersForEWO.list=list;
+	var clientContext = SP.ClientContext.get_current();
+	var oList = clientContext.get_web().get_lists().getByTitle('PAD&Planners'); 
+	var camlQuery = new SP.CamlQuery();
+	
+								 camlQuery.set_viewXml(
+													  '<View><Query><Where><Eq><FieldRef Name=\'PAD_x0020_NO\'/>' +
+													  '<Value Type=\'Text\'>' + PAD + '</Value></Eq></Where></Query>' +
+													  '</View>'
+													  );
+	collListItem = oList.getItems(camlQuery);
+	console.log(collListItem);
+	clientContext.load(collListItem);
+	clientContext.executeQueryAsync(Function.createDelegate(this, this.onPADSuccess), Function.createDelegate(this, this.onPADFail));	
+}
+
+function onPADSuccess(){
+	
+	var list=engineersForEWO.list;
+	var listItemInfo = '';
+	var listItemEnumerator = collListItem.getEnumerator();
+	while (listItemEnumerator.moveNext()) 
+		{
+		
+		var oListItem = listItemEnumerator.get_current();
+		
+		if(!oListItem.get_item(list)){
+			alert("Please check the number PAD for this workbook - nothing is shown in PAD Planners");
+			$( "#validationMessage" ).append( "<p clas='errorInfo'>Refer to <a href='https://share.opel.com/sites/MEACEWO/Lists/PADPlanners/All%20Items.aspx' target='_blank'>PAD Planners on SP</a> to check the issue</p>" );
+			$("#validationMessage").show();
+			break;
+		}
+		//var planner=oListItem.get_item(list)[0].$4K_1;
+		console.log(oListItem.get_item(list)[0]);
+		var plannerMail=oListItem.get_item(list)[0].$6_2;	
+		//var processor=oListItem.get_item('MPD')[0].$4K_1;
+		var processorMail=oListItem.get_item('MPD')[0].$6_2;
+		//var gl=oListItem.get_item('GL')[0].$4K_1;
+		var glMail=oListItem.get_item('GL')[0].$6_2;
+		
+		if(oListItem.get_item(list).length==2){;
+				var plannerMail2=oListItem.get_item(list)[1].$6_2;
+				var glMail2=oListItem.get_item('GL')[1].$6_2;
+				}
+		}		
+			engineersForEWO.planner.push(plannerMail);
+			engineersForEWO.processor.push(processorMail);
+			engineersForEWO.gl.push(glMail);
+			
+		if(plannerMail2){
+			engineersForEWO.planner.push(plannerMail2);
+		}
+		if(glMail2){
+			engineersForEWO.gl.push(glMail2);
+		}
+		console.log(engineersForEWO);
+		SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickerMePlaner'+"_TopSpan"].AddUserKeys(engineersForEWO.planner.join(";"));
+		SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickerMPDProcessor'+"_TopSpan"].AddUserKeys(engineersForEWO.processor.join(";"));
+		SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickerMeGL'+"_TopSpan"].AddUserKeys(engineersForEWO.gl.join(";"));
+}
+
+function onPADFail(sender, args){
+	alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+}
+
+function createSummary(){
+	mailReceivers.name=[];
+	cc.name=[];
+	mailReceivers.mail=[];
+	cc.mail=[];
+	platforms=[];
+	platformCoordinators=[];
+	
+	$(".mailReceivers").val("");
+	$(".mailCopy").val("");
+	secondValidation();
+
+
+	var arr=[".planner",".processor",".GL",".workbook"];
+	for(var i=0;i<arr.length;i++){
+		appendSummaryData(arr[i]);
+	}
+
+	checkIfVAANeedsToBeSend();
+	
+	var	mailsReceiver=mailReceivers.mail.join(";");
+	var mailsCC=cc.mail.join(";");
+	var platformCoords=platformCoordinators.join(";");
+
+	//moje
+	var PeoplePickerSummary = {'PlatformCoordinators': [], 'Processors': [], 'Planners': [], 'GL': []};
+	PeoplePickerSummary['TO'].push('peoplePickerDRE_TopSpan');
+
+	$.map($('.planner'), function(n){PeoplePickerSummary['Planners'].push(n.id);});
+	$.map($('.processor'), function(n){PeoplePickerSummary['Processors'].push(n.id);});
+	$.map($('.GL'), function(n){PeoplePickerSummary['GL'].push(n.id);});
+	//PlatformCoordinators ?
+
+	getUserInfo(SPClientPeoplePicker.SPClientPeoplePickerDict[PeoplePickerSummary['Planners'][0]]); //poprawić
+
+	SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickermailReceivers'+"_TopSpan"].AddUserKeys(mailsReceiver);
+	SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickermailMailCopy'+"_TopSpan"].AddUserKeys(mailsCC);
+	SPClientPeoplePicker.SPClientPeoplePickerDict['peoplePickermailPlatformCoordinators'+"_TopSpan"].AddUserKeys(platformCoords);
+
+	//createMailToVAA();
+}
+
+
+function checkIfVAANeedsToBeSend(){
+	if($("#infoVAA").is(":checked")){
+		if($(".frontAxle").is(":checked")){
+			cc.mail.push("sebastian.maier@opel.com");
+		}
+		if($(".rearAxle").is(":checked")){
+			cc.mail.push("franz.sabo@opel.com");
+		}
+		if($(".plantVAA").is(":checked")){
+			cc.mail.push("jacek.pedrycz@opel.com");
+		}
+	}
+}
+
+function secondValidation(){
+	var validation2=true;
+	if($("#staticTorque").is(":checked")){
+		var arrWithClass =[".modelYear",".workbook",".platform"];
+	}else{
+		var arrWithClass =[".modelYear",".workbook",".platform",".planner",".processor",".GL"];
+	}
+	for (var i=0;i<arrWithClass.length;i++){
+			validation2 = iterateOverClass(arrWithClass[i]);
+		if (!validation2){
+			return false;
+		}
+	}
+	if(validation2){
+		$(".summaryBtn").show();
+		$(".summaryInfo").show();
+		$(".createBtn").show();
+	}
+}
+
+function iterateOverClass(className){
+	var validation=true;
+	$(className).each(function(){
+			if(this.value!=""){ 
+			}else{
+				alert("Please fulfill necessary data");
+				validation=false;
+				return false;
+			}
+		});
+		return validation;
+}
+
+function checkIfIsInside(array,element){
+	if (array.length==0){
+		array.push(element);
+	}
+	else{
+		if(array.indexOf(element)>=0){
+		//do nothing
+		}else{
+		array.push(element);
+		}
+	}
+	return array;
+}
+
+function appendSummaryData(selector){
+	$(selector).each(function(index){
+		if(selector==".planner"){
+			var plannerField=this.value;
+			console.log('plannerField is:' + plannerField);
+			console.log(index);
+				if ((plannerField).indexOf(";")!=-1){
+						var res = plannerField.split(";");
+						for(var i=0;i<res.length;i++){
+							mailReceivers.mail=checkIfIsInside(mailReceivers.mail,res[i]);
+						}
+				}
+				else{
+					mailReceivers.mail=checkIfIsInside(mailReceivers.mail,this.value);
+					}				
+		}
+		else if(selector==".workbook"){
+			platforms=checkIfIsInside(platforms,this.value);
+		}
+		else{
+			var ccField=this.value;
+					if ((ccField).indexOf(";")!=-1){
+						var res = ccField.split(";");
+						for(var i=0;i<res.length;i++){
+							cc.mail=checkIfIsInside(cc.mail,res[i]);
+						}
+					}
+					else{
+						cc.mail=checkIfIsInside(cc.mail,this.value);
+					}
+			}
+		
+	})
+		for(var i=0;i<platforms.length;i++){
+			console.log(platforms[i]);
+			determinePlatformCoordinators(platforms[i])
+		}
+	};
+
+function determinePlatformCoordinators(workbook){
+	var mails=[];
+	switch(workbook){
+		case "Z": 
+				mails=["francesco.cau@opel.com","guenter.andreas.schieb@opel.com"," frank.sauer@opel."];
+				 break;
+		case "P3/P7":
+				mails=["guido.wagner@opel.com","angela.dudek@opel.com ","cristina.gonzalez.alcala@opel.com","bernd.langer@opel.com"];
+				 break;
+		case "J": 
+				mails=["guido.wagner@opel.com","cristina.gonzalez.alcala@opel.com"];
+				break;
+		case "0M":
+		case "0X":
+		case "0S":
+				mails=["cristina.gonzalez.alcala@opel.com","bernd.langer@opel.com","guido.wagner@opel.com"];
+				 break;
+		case "B": 
+				mails=["olaf.recha@opel.com", "marcus.sattler-schneider@opel.com","willi.blohm@opel.com","przemyslaw.wloka@opel.com","rui.canteiro@opel.com"];
+				break;
+		case "P(all plants)":
+				mails=["olaf.recha@opel.com","willi.blohm@opel.com","przemyslaw.wloka@opel.com","rui.canteiro@opel.com"];
+				break;
+		case "0W":
+				mails=["dieter.regner@opel.com","jacek.pedrycz@opel.com","przemyslaw.wloka@opel.com","zofia.guzy@opel.com"];
+				break;
+		}
+
+		for(var i=0;i<mails.length;i++){
+			platformCoordinators=checkIfIsInside( platformCoordinators,mails[i]);
+		}
+}
