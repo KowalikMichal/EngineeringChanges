fc649134ba4d4f26207f7dfdb4a4839ed0695b0d
Michał Kowalik <kowalik.michall@users.noreply.github.com>
2018-06-28 22:01:46 +0200

    vol 2

--- review.cs
+++ review.cs
@@ -5,8 +5,14 @@ namespace BoxApplication{
 		private double length;
 		private double breadth;
 
+		public Box(){
+			System.Console.WriteLine("Object is being created");
+		}
 		public void setLength(double len){
 			length = len;
+		}
+		public double displayLength(){
+			return length;
 		}
 
 		public void setBreadth(double bre){
@@ -15,6 +21,9 @@ namespace BoxApplication{
 
 		public double Calc(){
 			return length*breadth;
+		}
+		~Box(){
+			System.Console.WriteLine("object is being deleted")
 		}
 	}
 
@@ -26,6 +35,7 @@ namespace BoxApplication{
 
 			Box1.setLength(10.0);
 			Box1.setBreadth(10.0);
+			System.Console.WriteLine(Box1.displayLength());
 
 			Box2.setLength(13.12);
 			Box2.setBreadth(77.9);
@@ -42,3 +52,47 @@ https://www.tutorialspoint.com/csharp/csharp_classes.htm
 http://zajacmarek.com/2016/02/kurs-c-cz-5-wlasciwosci-properties/
 
 */
+
+namespace StaticVarApplication {
+   class StaticVar {
+	  public static int num;
+	  
+	  public void count() {
+		 num++;
+		 Console.WriteLine(num);
+	  }
+	  public int getNum() {
+		 return num;
+	  }
+   }
+   class StaticTester {
+	  static void Main(string[] args) {
+		 StaticVar s1 = new StaticVar();
+		 
+		var jsonob = false;
+		Console.WriteLine(jsonob.GetType());
+		Console.WriteLine(5 & 2 );
+
+		 s1.count();
+		 s1.count();
+		 s1.count();
+		 StaticVar s2 = new StaticVar();
+		 s2.count();
+		 s2.count();
+		 s2.count();
+		 
+		 Console.WriteLine("Variable num for s1: \n {0}", s1.getNum());
+		 Console.WriteLine("Variable num for s2: \r {0}", s2.getNum());
+		 Console.ReadKey();
+
+		 /*
+infinite loop
+         for (; ; ) {
+            Console.WriteLine("Hey! I am Trapped");
+         }
+		 */
+	  }
+   }
+}
+
+//https://www.tutorialspoint.com/csharp/csharp_encapsulation.htm
